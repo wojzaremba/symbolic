1d77fd3a7df0609520210f7e409a6b6c5d2b5070
Wojciech Zaremba <woj.zaremba@gmail.com>
2013-12-23 14:22:12 -0500

    Some new figures.

---
+++ code/plots/ab.m
@@ -0,0 +1,28 @@
+times = zeros(14, 2);
+for nidx = 1:size(times, 1)
+    fprintf('nidx = %d\n', nidx);
+    n = 2^nidx;
+    k = n;
+    m = n;
+    A = randn(n, k);
+    B = randn(k, m);
+
+    tic;
+    q = sum(sum(A * B));
+    times(nidx, 1) = toc;
+
+    tic;    
+    w = sum(sum(A .* repmat(sum(B, 2), [1, n])'));
+    times(nidx, 2) = toc;
+    assert(abs(q - w) < 1e-4);
+end
+
+plot(2.^(5:14), times(5:end, 1), 'Color', 'red', 'LineStyle', '--', 'LineWidth', 2);
+hold on
+plot(2.^(5:14), times(5:end, 2), 'Color', 'blue', 'LineStyle', '-', 'LineWidth', 2);
+
+legend('Cubic time computation', 'Quadratic time computation', 'Location', 'NorthWest');
+
+set(gca,'FontSize', 16)
+xlabel('Size of matrices');
+ylabel('time (s)');
